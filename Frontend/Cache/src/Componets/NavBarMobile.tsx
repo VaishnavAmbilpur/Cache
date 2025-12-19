8299a3a00620abeeb7fca5ce41bb16680c151be5
VaishnavAmbilpur <vaishnavambilpur2006@gmail.com>
2025-12-19 12:44:30 +0530

    fix

--- Frontend/Cache/src/Componets/NavBarMobile.tsx
+++ Frontend/Cache/src/Componets/NavBarMobile.tsx
@@ -1,4 +1,4 @@
-import Logo from "../assets/Logo.png"
+import Logo from "/src/assets/Logo.png";
 import { FaXTwitter } from "react-icons/fa6";
 import { FiYoutube } from "react-icons/fi";
 import { IoLinkSharp } from "react-icons/io5";
